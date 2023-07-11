bb69fad16a0fa25c54c2dd75ddb323fd20402a3c
Rada <rada.bikreva@mail.ru>
2023-07-11 23:15:59 +0500

    Added info

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+Console.Write("Введите трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("2-ая цифра введенного Вами числа -> "+stringNumber[1]);
